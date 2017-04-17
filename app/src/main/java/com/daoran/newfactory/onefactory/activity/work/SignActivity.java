4e2fcba14e57c345a1b51694d23ba2aa47155d64
lizhipeng123 <1036886274@qq.com>
2017-04-17 17:06:50 +0800

    2017-4-17 完善外勤签到

--- app/src/main/java/com/daoran/newfactory/onefactory/activity/work/SignActivity.java
+++ app/src/main/java/com/daoran/newfactory/onefactory/activity/work/SignActivity.java
@@ -1,5 +1,6 @@
 package com.daoran.newfactory.onefactory.activity.work;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.Context;
@@ -7,8 +8,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -21,6 +24,7 @@ import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
@@ -31,8 +35,13 @@ import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.UiSettings;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.CameraPosition;
 import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.maps2d.model.MyLocationStyle;
 import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.core.PoiItem;
 import com.amap.api.services.poisearch.PoiResult;
 import com.amap.api.services.poisearch.PoiSearch;
 import com.daoran.newfactory.onefactory.R;
@@ -60,13 +69,18 @@ import java.util.Date;
 import java.util.List;
 
 import okhttp3.Call;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.RuntimePermissions;
 
 /**
  * 外勤签到
  * Created by lizhipeng on 2017/3/31.
  */
-
-public class SignActivity extends BaseFrangmentActivity implements View.OnClickListener,LocationSource,AMapLocationListener {
+@RuntimePermissions
+public class SignActivity extends BaseFrangmentActivity
+        implements View.OnClickListener, PoiSearch.OnPoiSearchListener,
+        LocationSource, AMapLocationListener, AMap.OnCameraChangeListener {
     private View view;
 
     private MapView mapView;
@@ -74,45 +88,50 @@ public class SignActivity extends BaseFrangmentActivity implements View.OnClickL
     private AMapLocationClient mapLocationClient;
     private AMapLocationClientOption mapLocationClientOption;
     private boolean isFirstLoc = true;
-    private LocationSource.OnLocationChangedListener mListener ;
+    private double longitude;
+    private double latitude;
+    private LocationSource.OnLocationChangedListener mListener;
 
     private ProgressDialog progressDialog = null;//搜索时进度条
     private PoiSearch.Query query;//poi查询类
     private PoiSearch poiSearch;//搜索
     private PoiSearch.SearchBound searchBound;
+    private List<PoiItem> poiItems;
     private int currentPage;//当前页面，从0开始
     private PoiResult poiResult;//poi返回的结果
     private String city = "";//搜索城市
+    private String deepType;//搜索类型
     private int juli = 1000;
     private LatLonPoint latLonPoint;
-    private Spinner spinner;
-
+    private Spinner spinner, spinnnerfileTune;
 
     private static final String TAG = "TAG";
-//    private MapView mapView = null;
-//    private BaiduMap mBaidumap;
-//    private boolean isFirstLoc = true;
+    public static final String KEY_LAT = "lat";
+    public static final String KEY_LNG = "lng";
+    public static final String KEY_DES = "des";
 
-    private Button btnCount, btnSignCancle, btnSignOk, btnfileTune;
+    private String cityCode;
+
+    private Button btnCount, btnSignCancle, btnSignOk;
     private TextView tvSignAddress, tvSignDate;
     private Spinner SpinnerSign, spinnerfineTune;
     private EditText etRemark;
     private ImageView ivSignBack;
     private ImageView topBg;
     private ScrollView scrollviewSign;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> locationList = new ArrayList<>();
+
 
     private List<SignDebugBean.DataBean> signBean = new ArrayList<SignDebugBean.DataBean>();
     private SignDebugBean dataBean;
-
-
-//    private MyLocationListener mMyLocationListener = null;
-//    private LocationClient mLocationClient = null;
 
     private int year, month, date, hour, minute, second;
     private SharedPreferences sp;
     private SPUtils spUtils;
     private CrameUtils crameUtils;
     private boolean isMe = false;
+    private MarkerOptions mMarkerOptions;
 
 
     @Override
@@ -122,20 +141,33 @@ public class SignActivity extends BaseFrangmentActivity implements View.OnClickL
         setContentView(R.layout.activity_sign);
         mapView = (MapView) findViewById(R.id.mapSigngaode);
         mapView.onCreate(savedInstanceState);
-        if(aMap==null){
+        if (aMap == null) {
             aMap = mapView.getMap();
             UiSettings settings = aMap.getUiSettings();//设置显示定位按钮 并且可以点击
             aMap.setLocationSource(this);//设置了定位的监听,这里要实现LocationSource接口
             settings.setMyLocationButtonEnabled(true);// 是否显示定位按钮
             aMap.setMyLocationEnabled(true);//显示定位层并且可以触发定位,默认是flase
         }
+        setUp();
+        mMarkerOptions = new MarkerOptions();
+        mMarkerOptions.draggable(false);//可拖放性
+        mMarkerOptions.icon(BitmapDescriptorFactory.fromResource(R.mipmap.guidepoint_red));
         getViews();
         initViews();
+        initSpinner();
+
         init();
+
         sp = this.getSharedPreferences("my_sp", Context.MODE_WORLD_READABLE);
-        tvSignAddress = (TextView) findViewById(R.id.tvSignAddress);
-        String tvaddress = sp.getString("gmapaddress", "");
-        tvSignAddress.setText(tvaddress);
+
+
+        SignActivityPermissionsDispatcher.startLocationWithCheck(this);
+    }
+
+    @NeedsPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void startLocation() {
+        //启动定位
+        mapLocationClient.startLocation();
     }
 
     /**
@@ -163,7 +195,6 @@ public class SignActivity extends BaseFrangmentActivity implements View.OnClickL
         btnSignCancle.setOnClickListener(this);
         btnSignOk.setOnClickListener(this);
         topBg.setOnClickListener(this);
-        btnfileTune.setOnClickListener(this);
         crameUtils = new CrameUtils();
     }
 
@@ -181,7 +212,7 @@ public class SignActivity extends BaseFrangmentActivity implements View.OnClickL
         SpinnerSign = (Spinner) findViewById(R.id.SpinnerSign);
         topBg = (ImageView) findViewById(R.id.topBg);
         spinnerfineTune = (Spinner) findViewById(R.id.spinnerfineTune);
-        btnfileTune = (Button) findViewById(R.id.btnfileTune);
+        spinnnerfileTune = (Spinner) findViewById(R.id.spinnnerfileTune);
         getSpinner();
         getDate();
 
@@ -189,26 +220,147 @@ public class SignActivity extends BaseFrangmentActivity implements View.OnClickL
     }
 
     private void init() {
-        mapLocationClient = new AMapLocationClient(getApplicationContext());
-        mapLocationClient.setLocationListener(this);
-        //初始化定位参数
-        mapLocationClientOption = new AMapLocationClientOption();
-        //设置定位模式为Hight_Accuracy高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
-        mapLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-        //设置是否返回地址信息（默认返回地址信息）
-        mapLocationClientOption.setNeedAddress(true);
-        //设置是否只定位一次,默认为false
-        mapLocationClientOption.setOnceLocation(false);
-        //设置是否强制刷新WIFI，默认为强制刷新
-        mapLocationClientOption.setWifiActiveScan(true);
-        //设置是否允许模拟位置,默认为false，不允许模拟位置
-        mapLocationClientOption.setMockEnable(false);
-        //设置定位间隔,单位毫秒,默认为2000ms
-        mapLocationClientOption.setInterval(2000);
-        //给定位客户端对象设置定位参数
-        mapLocationClient.setLocationOption(mapLocationClientOption);
-        //启动定位
-        mapLocationClient.startLocation();
+        if (mapLocationClient == null) {
+            mapLocationClient = new AMapLocationClient(getApplicationContext());
+            mapLocationClient.setLocationListener(this);
+            //初始化定位参数
+            mapLocationClientOption = new AMapLocationClientOption();
+            //设置定位模式为Hight_Accuracy高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+            mapLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置是否返回地址信息（默认返回地址信息）
+            mapLocationClientOption.setNeedAddress(true);
+            //设置是否只定位一次,默认为false
+            mapLocationClientOption.setOnceLocation(false);
+            //设置是否强制刷新WIFI，默认为强制刷新
+            mapLocationClientOption.setWifiActiveScan(true);
+            //设置是否允许模拟位置,默认为false，不允许模拟位置
+            mapLocationClientOption.setMockEnable(false);
+            //设置定位间隔,单位毫秒,默认为2000ms
+            mapLocationClientOption.setInterval(2000);
+            //设置定位回调监听
+            mapLocationClient.setLocationListener(new AMapLocationListener() {
+
+                @Override
+                public void onLocationChanged(AMapLocation amapLocation) {
+                    if (amapLocation != null) {
+                        if (amapLocation.getErrorCode() == 0) {
+                            //解析定位结果
+                            Log.i(TAG, amapLocation.getPoiName());
+                            latitude = amapLocation.getLatitude();
+                            longitude = amapLocation.getLongitude();
+//                            setTextWithColor("纬度 = " + latitude + "\n经度 = " + longitude, Color.BLACK);
+                            cityCode = amapLocation.getCity();
+                            doSearch();
+                            mapLocationClient.stopLocation();
+                        } else if (amapLocation.getErrorCode() == 12) {
+//                            setTextWithColor(amapLocation.getErrorInfo(), Color.RED);
+                            Toast.makeText(getApplicationContext(), "权限不足，请在设置中授予相应权限", Toast.LENGTH_SHORT).show();
+                        } else {
+//                            setTextWithColor(amapLocation.getErrorInfo(), Color.RED);
+                            Log.e(TAG, "error code = " + amapLocation.getErrorCode());
+                        }
+                    }
+                }
+            });
+            //给定位客户端对象设置定位参数
+            mapLocationClient.setLocationOption(mapLocationClientOption);
+            //启动定位
+            mapLocationClient.startLocation();
+        }
+//        MyLocationStyle myLocationStyle = new MyLocationStyle();
+//        myLocationStyle.myLocationIcon
+//                (BitmapDescriptorFactory.
+//                        fromResource(R.drawable.location_landmark));// 设置小蓝点的图标
+//        myLocationStyle.strokeColor(Color.BLACK);// 设置圆形的边框颜色
+//        myLocationStyle.radiusFillColor(Color.argb(100, 0, 0, 180));//圆形填充颜色
+//        myLocationStyle.strokeWidth(1.0f);//圆形的边框粗细
+//        aMap.setMyLocationStyle(myLocationStyle);
+//        aMap.setLocationSource(this);// 设置定位监听
+//        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+//        aMap.setMyLocationEnabled(true);//设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+
+    }
+
+    private void setUp() {
+        aMap.setLocationSource(this);
+        aMap.setOnCameraChangeListener(this);
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        aMap.setMyLocationEnabled(true);
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(17.5f));
+    }
+
+    private void initSpinner() {
+        final String[] str = new String[]{"SDK默认的deepType", "汽车服务", "汽车销售", "汽车维修",
+                "摩托车服务", "餐饮服务", "购物服务", "生活服务", "体育休闲服务", "医疗保健服务",
+                "住宿服务", "风景名胜", "商务住宅", "政府机构及社会团体", "科教文化服务", "交通设施服务",
+                "金融保险服务", "公司企业", "道路附属设施", "地名地址信息", "公共设施"};
+
+        spinnnerfileTune.setAdapter(new ArrayAdapter<>(SignActivity.this, android.R.layout.simple_spinner_dropdown_item, str));
+        spinnnerfileTune.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        if (position == 0) {
+                            deepType = str[5] + "|" + str[7] + "|" + str[12];
+                        } else {
+                            deepType = (String) spinnnerfileTune.getSelectedItem();
+                        }
+                        doSearch();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                }
+        );
+    }
+
+    private void initSign() {
+        spinnerfineTune.setAdapter(new ArrayAdapter<>(SignActivity.this, android.R.layout.simple_spinner_dropdown_item, locationList));
+        spinnerfineTune.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                spUtils.put(SignActivity.this, "position", position);
+                String str = (String) spinnerfineTune.getSelectedItem();
+                spUtils.put(SignActivity.this, "addressItem", str);
+                tvSignAddress = (TextView) findViewById(R.id.tvSignAddress);
+                tvSignAddress.setText(str);
+                ToastUtils.ShowToastMessage("position" + position, SignActivity.this);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void doSearch() {
+//        loading();
+        aMap.setOnMapClickListener(null);//进行poi搜索时清除掉地图点击事件
+        query = new PoiSearch.Query("", deepType, cityCode);
+        PoiSearch poiSearch = new PoiSearch(this, query);
+        poiSearch.setOnPoiSearchListener(this);
+        query.setPageSize(20);// 设置每页最多返回多少条数据
+        query.setPageNum(0);//设置查询页码
+        LatLonPoint lp = new LatLonPoint(latitude, longitude);
+        poiSearch.setBound(new PoiSearch.SearchBound(lp, 2000, true));//设置周边搜索的中心点以及半径
+        //keyWord表示搜索字符串，
+        //第二个参数表示POI搜索类型，二者选填其一，
+        //POI搜索类型共分为以下20种：汽车服务|汽车销售|
+        //汽车维修|摩托车服务|餐饮服务|购物服务|生活服务|体育休闲服务|医疗保健服务|
+        //住宿服务|风景名胜|商务住宅|政府机构及社会团体|科教文化服务|交通设施服务|
+        //金融保险服务|公司企业|道路附属设施|地名地址信息|公共设施
+        //cityCode表示POI搜索区域，可以是城市编码也可以是城市名称，也可以传空字符串，空字符串代表全国在全国范围内进行搜索
+
+        poiSearch.searchPOIAsyn();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        SignActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 
     /**
@@ -315,9 +467,6 @@ public class SignActivity extends BaseFrangmentActivity implements View.OnClickL
             case R.id.topBg:
                 selectPic(view);
                 break;
-            case R.id.btnfileTune:
-
-                break;
         }
     }
 
@@ -372,7 +521,7 @@ public class SignActivity extends BaseFrangmentActivity implements View.OnClickL
     protected void onDestroy() {
         super.onDestroy();
 //        if (mapView != null) {
-            mapView.onDestroy();
+        mapView.onDestroy();
 //            mapView = null;
 //        }
         mapLocationClient.onDestroy();//销毁定位客户端。
@@ -397,7 +546,7 @@ public class SignActivity extends BaseFrangmentActivity implements View.OnClickL
         String Latitude = sp.getString("latitude", "");
         String languages = sp.getString("languages", "");
         String picstr = sp.getString("picurl", "");
-        String address = sp.getString("gmapaddress", "");
+        String address = sp.getString("addressItem", "");
         if (NetWork.isNetWorkAvailable(this)) {
 
             OkHttpUtils.post()
@@ -508,11 +657,11 @@ public class SignActivity extends BaseFrangmentActivity implements View.OnClickL
                             + aMapLocation.getStreet() + ""
                             + aMapLocation.getStreetNum());
                     StringBuffer buffer1 = new StringBuffer();
-                    buffer1.append(aMapLocation.getLatitude()+","+aMapLocation.getLongitude());
+                    buffer1.append(aMapLocation.getLatitude() + "," + aMapLocation.getLongitude());
 
-                    spUtils.put(SignActivity.this,"gmapaddress",buffer.toString());
-                    spUtils.put(SignActivity.this,"latitude",buffer1.toString());
-                    ToastUtils.ShowToastMessage(buffer.toString(),SignActivity.this);
+                    spUtils.put(SignActivity.this, "gmapaddress", buffer.toString());
+                    spUtils.put(SignActivity.this, "latitude", buffer1.toString());
+                    ToastUtils.ShowToastMessage(buffer.toString(), SignActivity.this);
                     isFirstLoc = false;
                 }
             } else {
@@ -520,8 +669,64 @@ public class SignActivity extends BaseFrangmentActivity implements View.OnClickL
                 Log.e("AmapError", "location Error, ErrCode:"
                         + aMapLocation.getErrorCode() + ", errInfo:"
                         + aMapLocation.getErrorInfo());
-                ToastUtils.ShowToastMessage("定位失败",SignActivity.this);
+                ToastUtils.ShowToastMessage("定位失败", SignActivity.this);
             }
         }
     }
+
+    @Override
+    public void onCameraChange(CameraPosition cameraPosition) {
+
+    }
+
+    @Override
+    public void onCameraChangeFinish(CameraPosition cameraPosition) {
+
+    }
+
+    @Override
+    public void onPoiSearched(PoiResult result, int rCode) {
+        locationList.clear();
+        if (rCode == 1000) {
+            if (result != null && result.getQuery() != null) {// 搜索poi的结果
+                if (result.getQuery().equals(query)) {// 是否是同一条
+                    PoiResult poiResult = result;
+                    ArrayList<PoiItem> poiItems = poiResult.getPois();
+
+                    if (poiItems != null && poiItems.size() > 0) {
+                        for (PoiItem p : poiItems) {
+                            Log.i(TAG, "getTitle = " + p.getTitle());
+                            locationList.add(p.getTitle() + "(" + p.getSnippet() + ")");
+                            String snippet = p.getSnippet();
+                            spUtils.put(SignActivity.this, "snippet", snippet);
+                            initSign();
+                            selectPositon();
+
+                        }
+
+                    }
+                } else {
+                    Log.e(TAG, "无结果");
+                }
+            }
+
+        } else if (rCode == 27) {
+            Log.e(TAG, "error_network");
+        } else if (rCode == 32) {
+            Log.e(TAG, "error_key");
+        } else {
+            Log.e(TAG, "error_other：" + rCode);
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem poiItem, int i) {
+
+    }
+
+    private void selectPositon() {
+
+    }
+
 }
